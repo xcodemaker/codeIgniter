bbef1f9ee3727903e995992e858ca6dc1cb287a2
dhammika Saman Kumara <dhammika.dsk@gmail.com>
2018-06-18 19:14:57 +0530

    add login page to project

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -4,8 +4,10 @@ class Home extends CI_Controller{
 
     public function index(){
         
-        $this->load->view('home_view');
+        $data['main_view']="home_view";
+
+        $this->load->view('layouts/main',$data);
     }
 }
 
-?>
+?> 
--- application/controllers/users.php
+++ application/controllers/users.php
@@ -2,45 +2,49 @@
 
 class Users extends CI_Controller{
 
-    public function show($user_id){
+    // public function show($user_id){
         
-        // $this->load-model('user_model');
+    //     // $this->load-model('user_model');
 
-        $data['result'] = $this->user_model->get_users($user_id);
+    //     $data['result'] = $this->user_model->get_users($user_id);
 
-        // foreach($result as $object){
-        //     echo $object->id;
-        // }    
-        // $data['welcome']='Welcome to my page';
-        $this->load->view('user_view',$data);
-    }
+    //     // foreach($result as $object){
+    //     //     echo $object->id;
+    //     // }    
+    //     // $data['welcome']='Welcome to my page';
+    //     $this->load->view('user_view',$data);
+    // }
 
-    public function insert(){
+    // public function insert(){
 
-        $username='shashini';
-        $password = '1235';
+    //     $username='shashini';
+    //     $password = '1235';
 
-        $this->user_model->create_users([
-            'username' => $username,
-            'password' => $password
-        ]);
-    }
+    //     $this->user_model->create_users([
+    //         'username' => $username,
+    //         'password' => $password
+    //     ]);
+    // }
 
-    public function update(){
-        $id=3;
-        $username='shashini bhagya';
-        $password = 'secret';
+    // public function update(){
+    //     $id=3;
+    //     $username='shashini bhagya';
+    //     $password = 'secret';
 
-        $this->user_model->update_users([
-            'username' => $username,
-            'password' => $password
-        ],$id);
-    }
+    //     $this->user_model->update_users([
+    //         'username' => $username,
+    //         'password' => $password
+    //     ],$id);
+    // }
 
-    public function delete(){
-        $id=2;
+    // public function delete(){
+    //     $id=2;
 
-        $this->user_model->delete_users($id);
+    //     $this->user_model->delete_users($id);
+    // }
+
+    public function login(){
+echo 'this is work';
     }
 }
 
